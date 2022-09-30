be6458e6d65d3f1ac3d7e9ca4e22b79e69976388
Virvon <dan130205@gmail.com>
2022-09-30 10:04:12 +0300

    exit cycle

---
+++ exit cycle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exit_cycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExit = false;
+
+            while(isExit == false)
+            {
+                Console.WriteLine("Для выхода из цикла напишите 'exit'.");
+                isExit = "exit" == Console.ReadLine();
+            }
+
+            Console.WriteLine("Вы вышли из цикла.");
+            Console.ReadKey();
+        }
+    }
+}
